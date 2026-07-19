f9a13f5989fa47f71a5db8cde2ecff1ae866ddc4
umfrancisco <francis.guitler@gmail.com>
2026-07-19 20:57:31 -0300

    Add production api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: "https://staging-api-public-security-a9e2.encr.app",
 });
 
 export const getCrimeData = async () => {
